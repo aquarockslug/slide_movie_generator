a44dc451178b72fd3cb2c59161c695a0f85389bb
aquarock <aqua.rock.slug@gmail.com>
2024-02-28 23:13:43 -0600

    get_paths rename

--- movie_maker.py
+++ movie_maker.py
@@ -15,14 +15,14 @@ def main():
 
 
 def merge_dir(source_dir):
-    def get_path(source_dir):
+    def get_paths(source_dir):
         for path in os.listdir(source_dir):
             yield f"{source_dir}/{path}"
 
     def merge(a, b):
         return a + b
 
-    return reduce(merge, get_path(source_dir))
+    return reduce(merge, get_paths(source_dir))
 
 
 if __name__ == "__main__":

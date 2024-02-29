df0c5a0a5c59d6a9c089e91a4aa07aa92416bf83
aquarock <aqua.rock.slug@gmail.com>
2024-02-28 23:12:13 -0600

    combined functions

--- movie_maker.py
+++ movie_maker.py
@@ -1,23 +1,29 @@
 import os, sys
 from functools import reduce
+
 # import pymovie
 
+
 def main():
-    source_dir = 'default'
+    source_dir = "default"
     if len(sys.argv) > 1:
         source_dir = sys.argv[1]
-    
-    print(merge_videos(source_dir))
 
-def get_path(source_dir):
-    for path in os.listdir(source_dir):
-        yield f'{source_dir}/{path}'
+    video = map(merge_dir, [source_dir])
 
-def merge_videos(source_dir):
+    print(list(video))
+
+
+def merge_dir(source_dir):
+    def get_path(source_dir):
+        for path in os.listdir(source_dir):
+            yield f"{source_dir}/{path}"
+
+    def merge(a, b):
+        return a + b
+
     return reduce(merge, get_path(source_dir))
 
-def merge(a, b):
-    return a + b
 
 if __name__ == "__main__":
     main()

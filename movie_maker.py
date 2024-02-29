3567e6aae7d11d6b0f0515faa924a3ba7d11c9e6
aquarock <aqua.rock.slug@gmail.com>
2024-02-28 22:46:40 -0600

    get path yield

--- movie_maker.py
+++ movie_maker.py
@@ -6,19 +6,15 @@ def main():
     source_dir = 'default'
     if len(sys.argv) > 1:
         source_dir = sys.argv[1]
+    
+    print(merge_videos(source_dir))
 
-    print(merge_videos([source_dir]))
+def get_path(source_dir):
+    for path in os.listdir(source_dir):
+        yield f'{source_dir}/{path}'
 
-def get_paths(paths, target_dir) -> list[str]:
-    for path in os.listdir(target_dir):
-        paths.append(f'{target_dir}/{path}')
-    return paths
-
-def merge_videos(source_dirs):
-    paths = []
-    for source_dir in source_dirs:
-        paths = get_paths(paths, source_dir)
-    return reduce(merge, paths)
+def merge_videos(source_dir):
+    return reduce(merge, get_path(source_dir))
 
 def merge(a, b):
     return a + b
